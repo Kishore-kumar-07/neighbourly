d7d02297da50b7e03949653f1a8ff6e2f8a83b2d
Kishore Kumar K <123319344+Kishore-kumar-07@users.noreply.github.com>
2025-10-11 11:11:22 +0530

    Update serviceProvider.go

--- backend/controllers/serviceProvider.go
+++ backend/controllers/serviceProvider.go
@@ -19,7 +19,10 @@ type Service struct {
 	Time string `json:"time"`
 	Description string `json:"description"`
 	Status string `json:"status"`
+	Seekername string `json:"seekername"`
+	Seekerphone string `json:"Seekerphone"`
 }
+
 
 func ViewServices (c * gin.Context){
 	var service []Service;
